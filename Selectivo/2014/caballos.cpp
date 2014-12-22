18d14b0e76468d20da4effddda6804f54ebb95b2
nikouni <nikouni@users.noreply.github.com>
2014-12-22 12:30:38 -0300

    Limpio y con comentarios

--- Selectivo/2014/caballos.cpp
+++ Selectivo/2014/caballos.cpp
@@ -1,28 +1,16 @@
+/*
+ Año: 2014
+ Certamen: Selectivo
+ Problema: 1
+ Tags: pd, ajedrez, tablero
+*/
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef vector<string> vs;
-typedef set<int> se;
-typedef pair<int,int> pii;
-typedef long long int tint;
-
-#define forsn(i,s,n) for(int i = (int)(s); i<((int)n); i++)
-#define dforsn(i,s,n) for(int i = (int)(n)-1; i>=((int)s); i--)
 #define forn(i,n) for(int i=0;i<(int)(n);i++)
-#define decl(v, c) typeof(c) v = c
-#define forall(i, c) for(decl(i, c.begin()); i!=c.end(); ++i)
-#define rall(c) (c).rbegin(), (c).rend()
-#define all(c) (c).begin(), (c).end()
-#define D(a) << #a << "=" << a << " "
-
-
 #define si(a) int((a).size())
-#define pb push_back
-#define mp make_pair
-
 
 const int MAXN = 1010;
 char T[MAXN][MAXN]; //Tablero
@@ -35,19 +23,26 @@ int F,C,M;
 int di[] = {-1,-2,-2,-1,1,2,2,1};
 int dj[] = {-2,-1,1,2,2,1,-1,-2};
 
+/*
+Implementación de la dinámica memoized.
+Retorna la cantidad de formas de armar la palabra W[pos..]
+partiendo de la casilla T[i][j] 
+*/
 int dp(int i,int j,int pos) {
 
     if (W[pos] != T[i][j]) return 0;
     if (pos == si(W)-1) return 1;
 
-
     int &res = DP[i][j][pos];
-    if (res != -1) return res;
+    if (res != -1) return res; //Si ya está calculado, lo retorno
 
     res = 0;
+    //Para cada uno de los 8 posibles movimientos del caballo
     forn(d,8) {
+		//Genero la nueva posicion (ni,nj)
         int ni = i + di[d];
         int nj = j + dj[d];
+        //Si la (ni,nj) está dentro del tablero, llamo recursivamente
         if (ni >= 0 && nj >= 0 && ni < F && nj < C)
             res = (res + dp(ni,nj,pos+1)) % M;
     }
@@ -55,44 +50,42 @@ int dp(int i,int j,int pos) {
     return res;
 }
 
-
-
+//Inicializo DP
 void init() {
-    //Inicializo DP
     forn(i,F)
         forn(j,C)
             forn(k,8)
                 DP[i][j][k] = -1;
-
 }
-
-
 
 int main () {
 	freopen("caballos.in","r",stdin);
-	//freopen("out.txt","w",stdout);
+	//freopen("caballos.out","w",stdout);
 
+	//Optimización Entrada/Salida
 	ios_base::sync_with_stdio(false);
 
-	cin >> F >> C >> M;
+	while(cin >> F >> C >> M) {
+		init();
 
-    init();
+		forn(i,F)
+			forn(j,C)
+				cin >> T[i][j];
 
-	forn(i,F)
-        forn(j,C)
-            cin >> T[i][j];
+		int res = 0;
 
+		forn(i,F)
+			forn(j,C)
+				res = (res + dp(i,j,0)) % M;
 
-    int res = 0;
-
-    forn(i,F)
-        forn(j,C)
-            res = (res + dp(i,j,0)) % M;
-
-    cout << res << endl;
-
-  return 0;
-
+		cout << res << endl;		
+	}
+    
+    return 0;
 }
 
-
+/*
+ 
+ Tiempo de ejecución: O(F * C * W.size())
+ 
+*/
