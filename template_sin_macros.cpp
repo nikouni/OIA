abd7d1fc15a19e0def307248a19aad11b2529fa8
nikouni <nikouni@users.noreply.github.com>
2014-12-28 20:30:55 -0300

    Template sin macros

---
+++ template_sin_macros.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+
+    //Optimización Entrada/Salida
+	ios_base::sync_with_stdio(false);
+
+  return 0;
+
+}
+
+
