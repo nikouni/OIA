3f450226e2d02066bb2257182a141c1c2ad666f4
nikouni <nikouni@users.noreply.github.com>
2014-12-28 20:30:37 -0300

    Template

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef set<int> se;
+typedef pair<int,int> pii;
+typedef long long int tint;
+
+#define forsn(i,s,n) for(int i = (int)(s); i<((int)n); i++)
+#define dforsn(i,s,n) for(int i = (int)(n)-1; i>=((int)s); i--)
+#define forn(i,n) for(int i=0;i<(int)(n);i++)
+#define decl(v, c) typeof(c) v = c
+#define forall(i, c) for(decl(i, c.begin()); i!=c.end(); ++i)
+#define rall(c) (c).rbegin(), (c).rend()
+#define all(c) (c).begin(), (c).end()
+#define D(a) << #a << "=" << a << " "
+#define si(a) int((a).size())
+#define pb push_back
+#define mp make_pair
+
+int main () {
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w",stdout);
+
+    //Optimización Entrada/Salida
+	ios_base::sync_with_stdio(false);
+
+  return 0;
+
+}
+
+
